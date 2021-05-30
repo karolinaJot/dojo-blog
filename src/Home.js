8c14775a60ceb9bfc88a7652c393fefa9f0bf505
karolinaJot <amicus.karo@gmail.com>
2021-05-30 11:27:08 +0200

    add styles

--- src/Home.js
+++ src/Home.js
@@ -1,29 +1,48 @@
 import styled from 'styled-components';
-import  { useState } from 'react';
+import { useState } from 'react';
 
 
 const Wrapper = styled.div``;
+
+const PreViewWrapper = styled.div`
+    padding: 10px 16px;
+    margin: 20px 0;
+    border-bottom: 1px solid #fafafa;
+    
+    &:hover {
+        box-shadow: 1px 3px 5px rgba(0, 0, 0, 0.1);
+    };
+
+    h2 {
+        font-size: 20px;
+        color: #f1356d;
+        margin-bottom: 8px;
+    };
+
+`;
 
 
 
 const Home = () => {
 
-    const [name, setName] = useState('Karolina');
-    const [age, setAge] = useState(33);
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website!', body: 'lorem ipsum...', author: 'Karolina', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'Maciej', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Weronika', id: 3 }
 
-    const handleClick = () => {
-        setName("Adam");
-        setAge(50);
-    }
- 
+    ]);
 
-    return ( 
+
+    return (
         <Wrapper>
-            <h2>Home Page</h2>
-            <p>{ name } ma { age } lat</p>   
-            <button onClick={handleClick}>Click me</button>
+            { blogs.map((blog) => (
+                <PreViewWrapper key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p> wirtten by {blog.author}</p>
+                </PreViewWrapper>
+            ))}
         </Wrapper>
-     );
+    );
 }
- 
+
 export default Home;
