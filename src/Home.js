a2f58651b69efa6f3eb0ad75ef389d82b7b2cd94
karolinaJot <amicus.karo@gmail.com>
2021-06-01 22:14:33 +0200

    add handling fetch errors

--- src/Home.js
+++ src/Home.js
@@ -11,25 +11,35 @@ const Wrapper = styled.div``;
 
 const Home = () => {
 
-    const [blogs, setBlogs] = useState([]);
+    const [blogs, setBlogs] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
 
 
     useEffect(() => {
         fetch('http://localhost:8000/blogs')
             .then( res => {
+                if(!res.ok) {
+                    throw Error('Could not fetch the data. Sorry');
+                }    
                 return res.json();
             })
             .then( data => {
                 setBlogs(data);
                 setIsLoading(false);
-                console.log(data);
+                setError(null);
+                
+            })
+            .catch( err => {
+                setIsLoading(false);
+                setError(err.message);
             })
     }, [])
 
     return (
         <Wrapper>
-            {/* --- sprawdza, czy lewa strona (blogs) jest true, jeśli nie, to w ogóle nie zjamuje się prawą stroną---- */}
+        {/* --- sprawdza, czy lewa strona (blogs) jest true, jeśli nie, to w ogóle nie zjamuje się prawą stroną---- */}
+            { error && <div>{ error }</div>}
            { isLoading && <div>Loading... </div>} 
            { blogs && <BlogsList blogs={blogs} title='All Blogs!' /> }
         </Wrapper>
